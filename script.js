cf07a85518e4ea1439febc3cfe6f1250734ea627
Natalie Anderson <natalieg.anderson@mail.utoronto.ca>
2025-01-22 16:16:46 -0500

    week 3
    week 3 commiting files

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("my-button");
+
+btn.addEventListener("click", () => { //objects that can fire events have an addEventListener()
+    console.log("You clicked me!");
+});
+
+
+
